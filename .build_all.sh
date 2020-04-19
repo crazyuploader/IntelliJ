7d6d061f5b68135ad18344ae98dab4590707fd7c
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-04-19 12:49:16 +0530

    Fix Builds; now switching to Apache Ant
    .build_all.sh
    .travis.yml
    build.txt
    build.xml
    build_all.sh

---
+++ .build_all.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Colors
+GREEN="\033[1;32m"
+NC="\033[0m"
+YELLOW="\033[1;33m"
+MAGENTA="\033[1;35m"
+RED="\033[0;31m"
+
+echo ""
+echo -e "${GREEN}" "Available File(s) -${NC}"
+if [[ ! -d "temp" ]]; then
+    mkdir temp
+fi
+LIST_FILES="$(find src/ -name '*.java')"
+echo "${LIST_FILES}"
+echo ""
+FILES=0
+ERROR_FILE=0
+echo -e "${YELLOW}Compiling with Javac${NC}"
+echo ""
+for f in ${LIST_FILES}; do
+    ((FILES = FILES + 1))
+    echo "Compiling '${f}'";
+    javac "${f}" -d ./temp
+    ERROR_CODE="$?"
+    if [[ ${ERROR_CODE} != "0" ]]; then
+        echo ""
+        echo -e "${RED}Error while compiling ${MAGENTA}'${f}'${NC}"
+        echo ""
+        ((ERROR_FILE = ERROR_FILE + 1))
+    fi
+done
+echo ""
+if [[ ${ERROR_FILE} != "0" ]]; then
+    echo -e "${YELLOW}Error(s) in some file(s).${NC}"
+    echo ""
+    echo -e "No. of file(s) checked: ${GREEN}${FILES}${NC}"
+    echo -e "No. of file(s) with error(s): ${RED}${ERROR_FILE}${NC}"
+    exit 1
+else
+    echo -e "No. of file(s) checked: ${GREEN}${FILES}${NC}"
+fi
+
+# Not in use
--- build_all.sh
+++ build_all.sh
@@ -4,40 +4,31 @@
 GREEN="\033[1;32m"
 NC="\033[0m"
 YELLOW="\033[1;33m"
-MAGENTA="\033[1;35m"
+# MAGENTA="\033[1;35m"
 RED="\033[0;31m"
 
 echo ""
 echo -e "${GREEN}" "Available File(s) -${NC}"
-if [[ ! -d "temp" ]]; then
-    mkdir temp
-fi
-LIST_FILES="$(find src/ -name '*.java')"
-echo "${LIST_FILES}"
-echo ""
 FILES=0
-ERROR_FILE=0
-echo -e "${YELLOW}Compiling with Javac${NC}"
-echo ""
+LIST_FILES="$(find src/ -name '*.java')"
 for f in ${LIST_FILES}; do
     ((FILES = FILES + 1))
-    echo "Compiling '${f}'";
-    javac "${f}" -d ./temp
-    ERROR_CODE="$?"
-    if [[ ${ERROR_CODE} != "0" ]]; then
-        echo ""
-        echo -e "${RED}Error while compiling ${MAGENTA}'${f}'${NC}"
-        echo ""
-        ((ERROR_FILE = ERROR_FILE + 1))
-    fi
+    echo "${f}"
 done
 echo ""
-if [[ ${ERROR_FILE} != "0" ]]; then
+echo -e "${YELLOW}Compiling with Javac using Ant${NC}"
+ant > build.txt
+ERROR_CODE="$?"
+if [[ ${ERROR_CODE} != "0" ]]; then
+    echo ""
+    echo -e "${RED}$(cat build.txt)${NC}"
+    echo ""
     echo -e "${YELLOW}Error(s) in some file(s).${NC}"
     echo ""
     echo -e "No. of file(s) checked: ${GREEN}${FILES}${NC}"
-    echo -e "No. of file(s) with error(s): ${RED}${ERROR_FILE}${NC}"
-    exit 1
 else
+    echo ""
+    cat build.txt
+    echo ""
     echo -e "No. of file(s) checked: ${GREEN}${FILES}${NC}"
 fi
