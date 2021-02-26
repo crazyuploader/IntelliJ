0eeda7051811c8d370335163992a9509cdb70023
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2021-02-26 12:18:08 +0530

    Create First.java

---
+++ src/com/java/PracticalFile/First.java
@@ -0,0 +1,13 @@
+package com.java.PracticalFile;
+
+class Demo {
+    Demo(){
+        System.out.print("Hello from constructor of class \"Demo\"");
+    }
+}
+
+public class First {
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+    }
+}
